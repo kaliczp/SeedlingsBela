10fe991f100199ad175c6a3f38d18f43cd03fc2e
Kalicz Péter <kaliczp@gmail.com>
2025-04-02 18:28:35 +0200

    Save tabular results

--- lm.R
+++ lm.R
@@ -8,3 +8,4 @@ for(ttvar in AllTypes[-1]){
     NewSeedl.lm.df <- cbind(Var = ttvar, as.data.frame(ttact.sum$coefficients[2,c(1,4), drop = FALSE]))
     Seedl.lm.df <- rbind(Seedl.lm.df, NewSeedl.lm.df)
 }
+write.table(Seedl.lm.df, "Meredek.csv", dec = ",", sep = ";", row.names = FALSE)
