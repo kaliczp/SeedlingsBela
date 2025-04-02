dd01a9c6903ecbcc39a3566b9b0148d29110de31
Kalicz Péter <kaliczp@gmail.com>
2025-04-02 19:12:15 +0200

    Rounding is applied

--- lm.R
+++ lm.R
@@ -2,23 +2,23 @@ ttvar <- AllTypes[1]
 ttact.lm <- lm(Price ~ Year, Reshaped[Reshaped$Type == ttvar,])
 ttact.sum <- summary(ttact.lm)
 ttslope <- as.data.frame(ttact.sum$coefficients[2,c(1,4), drop = FALSE])
-ttconfint <- as.data.frame(confint(ttact.lm, 'Year', level=0.95))
+ttconfint <- round(as.data.frame(confint(ttact.lm, 'Year', level=0.95)), 3)
 Seedl.lm.df <- data.frame(Var = ttvar,
-                          Estimate = ttslope$Estimate,
+                          Estimate = round(ttslope$Estimate,3),
                           Lower.ci = ttconfint[,1],
                           Upper.ci = ttconfint[,2],
-                          p.val = ttslope[,2]
+                          p.val = round(ttslope[,2],5)
                           )
 for(ttvar in AllTypes[-1]){
     ttact.lm <- lm(Price ~ Year, Reshaped[Reshaped$Type == ttvar,])
     ttact.sum <- summary(ttact.lm)
     ttslope <- as.data.frame(ttact.sum$coefficients[2,c(1,4), drop = FALSE])
-    ttconfint <- as.data.frame(confint(ttact.lm, 'Year', level=0.95))
+    ttconfint <- round(as.data.frame(confint(ttact.lm, 'Year', level=0.95)),3)
     NewSeedl.lm.df <- data.frame(Var = ttvar,
-                                 Estimate = ttslope$Estimate,
+                                 Estimate = round(ttslope$Estimate,3),
                                  Lower.ci = ttconfint[,1],
                                  Upper.ci = ttconfint[,2],
-                                 p.val = ttslope[,2]
+                                 p.val = round(ttslope[,2],5)
                                  )
     Seedl.lm.df <- rbind(Seedl.lm.df, NewSeedl.lm.df)
 }
