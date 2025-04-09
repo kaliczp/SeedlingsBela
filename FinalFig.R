57882f1099a104b2e3d0053a6a8afb4b789ccc92
Kalicz Péter <kaliczp@gmail.com>
2025-04-09 16:08:30 +0200

    Jpeg creatin with small adjusment of labels

--- FinalFig.R
+++ FinalFig.R
@@ -8,9 +8,11 @@ Selected$Source <- factor(Selected$Source,
                           )
 Selected$Type <- factor(Selected$Type, labels = c("Q. robur 2/1", "Populus 1/0", "Acer 1/0"))
 
+jpeg(width = 15, height = 10, units = "cm", res = 300)
 ggplot(data = Selected, aes(x=Year, y = Price, group = Type)) +
     geom_point(aes(colour = Source)) +
     geom_smooth(method = "lm") +
     facet_wrap(~Type) +
     scale_x_continuous(breaks = c(2008, 2010, 2012, 2014, 2016)) +
-    theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+dev.off()
