eeb9942f543381dbb0aabd5dd267a702c421ce8a
Kalicz Péter <kaliczp@gmail.com>
2025-04-09 13:52:09 +0200

    Select significant trends

---
+++ FinalFig.R
@@ -0,0 +1,6 @@
+levels(Reshaped$Type)
+Selected <- Reshaped[Reshaped$Type == "KST 2/0 MCS" |
+                     Reshaped$Type == "SZNY 1/0 MCS"|
+                     Reshaped$Type == "Juhar 1/0 MCS", ]
+ggplot(data = Selected, aes(x=Year, y = Price, group = Type)) + geom_point(aes(colour = Source)) + geom_smooth(method = "lm") + facet_wrap(~Type) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
